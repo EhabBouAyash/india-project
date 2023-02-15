18bfaaf3578857f49b20c00ce38b33ead2c88398
Dmitry <dzinoviev@gmail.com>
2023-02-15 00:14:20 -0500

    Added tools for HW3.

--- src/wish.c
+++ src/wish.c
@@ -1,22 +1,26 @@
+#include <limits.h>
 #include "wish.h"
+
 int main(int argc, char *argv[])
 {
   // These two lines make the macOS C compiler happy.
   // Otherwise, it complains about unused parameters.
-  
-  argc = 0;
-  argv = NULL;
-  int ok_if_missing = 0; 
-  char* home = getenv("HOME");
-  char * path= strcat(home, "/wish.cfg");
+  (void)argc;
+  (void)argv;
 
-  FILE * file = fopen(path,"r");
-  if (!file){
-    ok_if_missing =1;
-    fclose(file);
-  }
-  printf("%s",home);
-  printf("%s", WISH_DEFAULT_PROMPT);
-  wish_read_config(path,ok_if_missing);
+  char path[PATH_MAX];
+  char *home = getenv("HOME");
+#ifdef DEBUG
+  home = "."; // So that you could place the config into the CWD
+#endif
+  sprintf(path, "%s/%s", (home ? home : "."), WISH_CONFIG);
+  wish_read_config(path, 1);
+  
+  // This is just a skeleton for your convenience
+  fputs(WISH_DEFAULT_PROMPT, stdout);
+  char *line = wish_read_line(stdin);
+  if(line)
+    free(line);
+  
   return EXIT_SUCCESS;
 }
--- src/wish_read.c
+++ src/wish_read.c
@@ -1,62 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
 #include "wish.h"
-int lastchar = 0;
-int l =1;
+
 char *wish_read_line(FILE *in) {
-  
+  char buffer[WISH_MAX_INPUT + 2] = ""; // truncate the buffer
 
+  // Get a string and check its length
+  fgets(buffer, WISH_MAX_INPUT + 2, in);
+  if(strlen(buffer) > WISH_MAX_INPUT) {
+    fputs("wish: line too long\n", stderr);
 
-  //read to EOF or until line break 
-  //if it reaches 80 characters and theres no line break, read to line break and discard everything
-  //if it reaches before the 80, get it into the buffer and stop filling the buffer and print it. then read to  the next line break and discard everything else
-  int linecount=1;
-  if (in ==NULL){
-    perror("File doesn't exist");
-    exit(1);
+    // Clean the rest of the line
+    int c = fgetc(in);
+    while (c != '\n' && c != EOF)
+      c = fgetc(in);
+    return NULL;
   }
 
-  int len = WISH_MAX_INPUT;
-  size_t bufflen = len; 
-  char * buff = malloc(sizeof(char)*len);
-  buff[0] = '\0';
-  while (getline(&buff, &bufflen, in)!=-1){
-    if ((strchr(buff, '\n')) && (int)strlen(buff)<=WISH_MAX_INPUT){
-      if (!isspace(buff[0])){
-        printf("\nLine %d: \n%s",linecount++, buff);
-      }
-    }
-    
-    else if ((int)strlen(buff)<=WISH_MAX_INPUT){
-      printf("\nLine %d: \n%s",linecount++, buff);
-    }
-    else{
-      printf("wish: line too long");
-      buff[0] = '\0';
+  // Trim the line
+  strtok(buffer, "\n");
+
+  // Check the line for being blank
+  for(size_t i = 0; i < strlen(buffer); ++i) {
+    if(!isspace(buffer[i])) {
+      // Alloate memory
+      char *line = malloc(strlen(buffer) + 1);
+      if (!line) // Too bad
+	abort();
+      strcat(line, buffer);
+      return line;
     }
   }
-  
 
-  fclose(in);
-  free(buff);
-
-
-
-  //do the same for command line reading 
-  
   return NULL;
 }
 
-
-//1 if missing
-//0 if its there
 int wish_read_config(char *fname, int ok_if_missing) {
-  FILE * file;
-  file = fopen(fname,"r");
-  if (ok_if_missing==0){
-    wish_read_line(file);
-    return EXIT_SUCCESS;
+  FILE *config;
+
+  // Check if the file exists
+  if(!(config = fopen(fname, "r"))) {
+    if (ok_if_missing)
+      return 0;
+    // Report missing
+    perror(fname);
+    return 1;
   }
-  else{
-    return EXIT_FAILURE;
+
+  // Read the file line by line
+  while(!feof(config)) {
+    char *line = wish_read_line(config);
+    if(line) {
+#ifdef DEBUG
+      fprintf(stderr, "%s\n", line); // Only for debugging
+#endif
+      free(line);
+    }
   }
+
+  fclose(config);
   return 0;
 }
